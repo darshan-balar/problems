d868335a3b9434d452c3145a10f43382799dabcb
darshan-balar <darshanbalar01@gmail.com>
2022-01-16 11:05:16 +0530

    Darshan-16,jan

---
+++ Welcome.c
@@ -0,0 +1,14 @@
+//1. problem is to print message on console
+
+//2. start->print->end
+
+
+#include<stdio.h> //header file
+
+//begin
+int main()
+{
+    printf("Welcome to World of programming\n"); // Welcome message
+    return 0;
+}//end
+
